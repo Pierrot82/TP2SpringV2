00b6ac483ecef3c056b63999085040d1ffb867d4
Pierrot82 <pierre.larroque.82@hotmail.fr>
2023-07-02 15:52:12 +0200

    creation du projet

---
+++ src/main/java/com/inti/Tp2SpringV2Application.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2SpringV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2SpringV2Application.class, args);
+	}
+
+}
